7898467d9b39675808a714f1a65191987ff8b5ac
MasterJH5574 <45167100+MasterJH5574@users.noreply.github.com>
2019-08-05 11:12:26 +0800

    version Aug 5 of 4 RPCs(initial) jhy tql! jjb tql! zzc tql! mxy tql!

--- src/kademlia/rpc.go
+++ src/kademlia/rpc.go
@@ -1,50 +1,108 @@
 package kademlia
 
 import (
-	"net/rpc"
+	"math/big"
+	"sort"
 	"time"
 )
 
 func (o *Node) RPCPing(p Contact, res *PingReturn) error {
-	// Todo: update k-bucket (goroutine?)
-	var success bool
-	for i := 0; i < 3; i++ {
-		chOK := make(chan bool)
-		go func() {
-			client, err := rpc.Dial("tcp", p.Ip)
-			if err == nil {
-				err = client.Close()
-				chOK <- true
-			} else {
-				chOK <- false
-			}
-		}()
-		select {
-		case ok := <-chOK:
-			if ok {
-				success = true
-			} else {
-				continue
-			}
-		case <-time.After(time.Second / 2):
-			break
-		}
-		if success == true {
-			break
-		}
-	}
-	res = &PingReturn{success, p}
+	go o.O.updateBucket(p)
+	*res = PingReturn{Contact{o.O.ID, o.O.IP}, true}
 	return nil
 }
 
 func (o *Node) RPCStore(obj StoreRequest, res *StoreReturn) error {
+	go o.O.updateBucket(obj.Header)
+	o.O.Data.lock.Lock()
+	if o.O.Data.Map[obj.Pair.Key] == nil {
+		o.O.Data.Map[obj.Pair.Key] = make(map[string]time.Time)
+	}
+	o.O.Data.Map[obj.Pair.Key][obj.Pair.Val] = obj.Expire
+	o.O.Data.lock.Unlock()
+	*res = StoreReturn{Contact{o.O.ID, o.O.IP}, true}
 	return nil
 }
 
-func (o *Node) RPCFindNode(p FindNodeRequest, res *FindNodeReturn) error {
+func (o *Node) RPCFindNode(arg FindNodeRequest, res *FindNodeReturn) error {
+	go o.O.updateBucket(arg.Header)
+	p := new(big.Int).Xor(arg.Id, o.O.ID).BitLen() - 1
+	o.O.kBuckets[p].mutex.Lock()
+	if o.O.kBuckets[p].size == bucketSize {
+		*res = FindNodeReturn{Contact{o.O.ID, o.O.IP}, o.O.kBuckets[p].arr[:]}
+		o.O.kBuckets[p].mutex.Unlock()
+		return nil
+	}
+	o.O.kBuckets[p].mutex.Unlock()
+	var arr []Contact
+	for i := 0; i < B; i++ {
+		o.O.kBuckets[i].mutex.Lock()
+		for j := 0; j < o.O.kBuckets[i].size; j++ {
+			arr = append(arr, o.O.kBuckets[i].arr[j])
+		}
+		o.O.kBuckets[i].mutex.Unlock()
+	}
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i].Id.Cmp(arr[j].Id) < 0
+	})
+	length := len(arr)
+	if length >= bucketSize {
+		*res = FindNodeReturn{Contact{o.O.ID, o.O.IP}, arr[:20]}
+	} else {
+		*res = FindNodeReturn{Contact{o.O.ID, o.O.IP}, arr[:]}
+	}
 	return nil
 }
 
-func (o *Node) RPCFindValue(p FindValueRequest, res *FindValueReturn) error {
+func (o *Node) RPCFindValue(arg FindValueRequest, res *FindValueReturn) error {
+	go o.O.updateBucket(arg.Header)
+
+	value, ok := o.O.getValue(arg.Key)
+	if ok {
+		*res = FindValueReturn{
+			Header:  Contact{o.O.ID, o.O.IP},
+			Closest: nil,
+			Val:     value,
+		}
+		return nil
+	}
+
+	p := new(big.Int).Xor(arg.HashId, o.O.ID).BitLen() - 1
+	o.O.kBuckets[p].mutex.Lock()
+	if o.O.kBuckets[p].size == bucketSize {
+		*res = FindValueReturn{
+			Header:  Contact{o.O.ID, o.O.IP},
+			Closest: o.O.kBuckets[p].arr[:],
+			Val:     nil,
+		}
+		o.O.kBuckets[p].mutex.Unlock()
+		return nil
+	}
+	o.O.kBuckets[p].mutex.Unlock()
+	var arr []Contact
+	for i := 0; i < B; i++ {
+		o.O.kBuckets[i].mutex.Lock()
+		for j := 0; j < o.O.kBuckets[i].size; j++ {
+			arr = append(arr, o.O.kBuckets[i].arr[j])
+		}
+		o.O.kBuckets[i].mutex.Unlock()
+	}
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i].Id.Cmp(arr[j].Id) < 0
+	})
+	length := len(arr)
+	if length >= bucketSize {
+		*res = FindValueReturn{
+			Header:  Contact{o.O.ID, o.O.IP},
+			Closest: arr[:20],
+			Val:     nil,
+		}
+	} else {
+		*res = FindValueReturn{
+			Header:  Contact{o.O.ID, o.O.IP},
+			Closest: arr[:],
+			Val:     nil,
+		}
+	}
 	return nil
 }
